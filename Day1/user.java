d47485c14193baab9c3d3c74dbbf577e53f8b883
Mudit-Tarway <official.mudittarway@gmail.com>
2025-08-10 08:24:47 +0530

    User oops questions coded in java

---
+++ Day1/user.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class user{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the month : ");
+        String month = sc.next();
+        monthInformation(month);
+        sc.close();
+    }
+    public static void monthInformation(String month){
+        month = month.toLowerCase();
+        switch(month){
+              case "january":
+                System.out.println("January has 31 days.");
+                break;
+            case "february":
+                System.out.println("February has 28 or 29 days.");
+                break;
+            case "march":
+                System.out.println("March has 31 days.");
+                break;
+            case "april":
+                System.out.println("April has 30 days.");
+                break;
+            case "may":
+                System.out.println("May has 31 days.");
+                break;
+            case "june":
+                System.out.println("June has 30 days.");
+                break;
+            case "july":
+                System.out.println("July has 31 days.");
+                break;
+            case "august":
+                System.out.println("August has 31 days.");
+                break;
+            case "september":
+                System.out.println("September has 30 days.");
+                break;
+            case "october":
+                System.out.println("October has 31 days.");
+                break;
+            case "november":
+                System.out.println("November has 30 days.");
+                break;
+            case "december":
+                System.out.println("December has 31 days.");
+                break;
+            default:
+                System.out.println("Invalid month name.");
+        }
+    } 
+}
