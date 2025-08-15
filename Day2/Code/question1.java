ff67fbcf86dbc4ad386d727378f60bd3dbd5839b
Mudit-Tarway <official.mudittarway@gmail.com>
2025-08-15 16:00:25 +0530

    Removed comment line1.

---
+++ Day2/Code/question1.java
@@ -0,0 +1,58 @@
+package Day2.Code;
+
+import java.util.*;
+class Student {
+    private String name;
+    private int roll_no;
+    private int year;
+    private String course;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRollNo(int roll_no) {
+        this.roll_no = roll_no;
+    }
+
+    public int getRollNo() {
+        return roll_no;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+}
+
+public class question1 {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setName("Shivam Kumar");
+        student.setRollNo(562);
+        student.setYear(2025);
+        student.setCourse("Computer Science");
+
+        // Printing values using getters
+        System.out.println("Name: " + student.getName());
+        System.out.println("Roll No: " + student.getRollNo());
+        System.out.println("Year: " + student.getYear());
+        System.out.println("Course: " + student.getCourse());
+    }
+}
--- Day2/Code/question2.java
+++ Day2/Code/question2.java
@@ -1,4 +1,4 @@
-// package Day2.Code;
+package Day2.Code;
 import java.util.*;
 public class question2 {
     public static void main(String arges[]){
